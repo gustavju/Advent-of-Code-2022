cc2afa08d74d382b5aec4fd19cd2c09c05f68fa8
gustavjunedahl <gustav.junedahl@gmail.com>
2022-12-06 08:37:34 +0100

    Day 6

---
+++ Days/Day6.cs
@@ -0,0 +1,20 @@
+namespace AoC2022.Days;
+
+public class Day6 : BaseDay
+{
+    private int GetIndexOfMarker(string s, int length)
+    {
+        int i = 0;
+        while (s[i..(i + length)].Distinct().Count() != length)
+        {
+            i++;
+        }
+        return i + length;
+    }
+    
+    public override string PartOne(string input)
+        => GetIndexOfMarker(input, 4).ToString();
+
+    public override string PartTwo(string input)
+        => GetIndexOfMarker(input, 14).ToString();
+}
